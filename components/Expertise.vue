974576742a85d85d52346e64afeba12d6024bb0f
zasalaston <zasalaston@DESKTOP-HN850JB.localdomain>
2023-11-16 08:07:47 -0500

    update tech skill

--- components/Expertise.vue
+++ components/Expertise.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="mt-8">
     <div class="mt-2">
-      <h2
-        class="mb-2 text-xl text-gray-700 font-bold dark:text-blue-200 capitalize"
-      >
+      <h2 class="mb-2 text-xl text-gray-700 font-bold dark:text-blue-200 capitalize">
         Frontend
       </h2>
 
@@ -16,6 +14,16 @@
           <Css class="h-10 w-10 rounded-sm drop-shadow-sm"></Css>
         </li> -->
         <li class="flex items-center">
+          <!-- <Symfony class="h-10 w-10"></Symfony> -->
+          <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg">
+
+  <image href="../assets/devicon/angular.svg"  height="40" width="40"/>
+</svg>
+        </li>
+        <li class="flex items-center">
+          <Angular class="h-10 w-10"></Angular>
+        </li>
+        <li class="flex items-center">
           <Bootstrap class="h-10 w-10"></Bootstrap>
         </li>
         <li class="flex items-center">
@@ -27,20 +35,39 @@
         <li class="flex items-center">
           <Vuejs class="h-10 w-10"></Vuejs>
         </li>
+
         <li class="flex items-center">
-          <Nuxt class="h-10 w-10"></Nuxt>
+          <React class="h-10 w-10"></React>
         </li>
+
       </ul>
     </div>
     <div class="mt-4">
-      <h2
-        class="mb-2 text-xl text-gray-700 font-bold dark:text-blue-200 capitalize"
-      >
+      <h2 class="mb-2 text-xl text-gray-700 font-bold dark:text-blue-200 capitalize">
         Backend
       </h2>
       <ul class="mb-6 flex flex-wrap gap-4">
         <li class="flex items-center">
           <Laravel class="h-10 w-10"></Laravel>
+        </li>
+
+        <li class="flex items-center">
+          <!-- <Symfony class="h-10 w-10"></Symfony> -->
+          <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg">
+  <image href="../assets/devicon/nest.svg"  height="40" width="40"/>
+</svg>
+        </li>
+        <li class="flex items-center">
+          <!-- <Symfony class="h-10 w-10"></Symfony> -->
+          <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg">
+  <image href="../assets/devicon/symfony.svg"  height="40" width="45"/>
+</svg>
+        </li>
+        <li class="flex items-center">
+          <Next class="h-10 w-10"></Next>
+        </li>
+        <li class="flex items-center">
+          <Nuxt class="h-10 w-10"></Nuxt>
         </li>
         <li class="flex items-center">
           <Node class="h-10 w-10"></Node>
@@ -59,7 +86,12 @@ import TailwindCss from "../assets/devicon/tailwindcss.svg?inline";
 import Nuxt from "../assets/devicon/nuxt.svg?inline";
 import Bootstrap from "../assets/devicon/bootstrap.svg?inline";
 import Laravel from "../assets/devicon/laravel.svg?inline";
+import Next from "../assets/devicon/next.svg?inline";
+import Nest from "../assets/devicon/nest.svg?inline";
+import React from "../assets/devicon/react.svg?inline";
+import Symfony from "../assets/devicon/symfony.svg?inline";
 import Node from "../assets/devicon/node.svg?inline";
+import Angular from "../assets/devicon/angular.svg";
 export default {
   components: {
     Javascript,
@@ -70,7 +102,8 @@ export default {
     TailwindCss,
     Nuxt,
     Laravel,
-    Node,
+    Node, Symfony, Next, Nest, React,
+    Angular
   },
 };
 </script>
