0d73774012a37226f554ba97ed46ba35a07cea0c
zasalaston <zasalaston@DESKTOP-HN850JB.localdomain>
2023-11-16 12:59:52 -0500

    update services, the view is good, for not in desktop

--- pages/services.vue
+++ pages/services.vue
@@ -2,7 +2,7 @@
   <div class="max-w-screen-xl mx-auto">
     <h2 class="text-3xl font-bold mb-6">Servicios Destacados</h2>
 
-    <div v-for="service in services" :key="service.id" class="mb-8">
+    <div v-for="service in services" :key="service.id" class="mb-8 bg-white p-6 rounded-lg shadow-md">
       <div class="flex flex-wrap mb-4">
         <span
           v-for="category in service.category"
@@ -21,29 +21,27 @@
         :media-alt="service.title || 'Media Alt Text'"
       >
         <template v-slot:content>
-          <div class="flex flex-col-reverse md:flex-row">
-            <div class="md:w-1/2 md:pr-6">
-              <p v-if="service.title" class="text-xl font-bold mb-2">{{ service.title }}</p>
-              <div class="flex flex-wrap mb-4">
-                <span
-                  v-for="tech in service.technologies"
-                  :key="tech"
-                  class="mr-2 mb-2 inline-block bg-green-500 text-white px-2 py-1 rounded-full"
-                >
-                  {{ tech }}
-                </span>
-              </div>
-              <p v-if="service.description" class="mb-4">{{ service.description }}</p>
-              <p v-if="service.content" class="mb-4">{{ service.content }}</p>
+          <div>
+            <p v-if="service.title" class="text-xl font-bold mb-2">{{ service.title }}</p>
+            <div class="flex flex-wrap mb-4">
+              <span
+                v-for="tech in service.technologies"
+                :key="tech"
+                class="mr-2 mb-2 inline-block bg-green-500 text-white px-2 py-1 rounded-full"
+              >
+                {{ tech }}
+              </span>
             </div>
-            <!-- Mostrar la imagen solo si hay un enlace -->
-            <img
-              v-if="service.mediaLink"
-              :src="service.mediaLink"
-              :alt="service.title"
-              class="w-full h-auto md:w-1/2 object-cover rounded-lg shadow-lg mb-4 md:mb-0"
-            />
+            <p v-if="service.description" class="mb-4 text-gray-700">{{ service.description }}</p>
+            <p v-if="service.content" class="mb-4 text-gray-700">{{ service.content }}</p>
           </div>
+          <!-- Mostrar la imagen solo si hay un enlace -->
+          <img
+            v-if="service.mediaLink"
+            :src="service.mediaLink"
+            :alt="service.title"
+            class="w-full h-auto max-w-full rounded-lg shadow-md mt-4"
+          />
         </template>
       </media-and-text>
     </div>
@@ -62,3 +60,6 @@ export default {
 };
 </script>
 
+<style scoped>
+/* No necesitas estilos adicionales aquí, ya que Tailwind CSS se encargará de eso */
+</style>
